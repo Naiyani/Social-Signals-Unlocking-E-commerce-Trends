a5f024c7901f5e0cbe1f45dbd9c7e57b79c88562
Naiyani <132041238+Naiyani@users.noreply.github.com>
2025-07-30 17:19:48 +0530

    Create validation.sql

---
+++ sql/validation.sql
@@ -0,0 +1,17 @@
+select * from orders limit 10;
+select count(*) from orders;
+
+select order_id, md5_hash, dv_load_timestamp from orders LIMIT 5;
+
+select order_id, count(*) from orders 
+GROUP BY order_id 
+HAVING count(*) > 1;
+
+select * from feedback LIMIT 10;
+select count(*) from feedback;
+
+select feedback_id, md5_hash, dv_load_timestamp from feedback LIMIT 5;
+
+select feedback_id, count(*) from feedback
+GROUP BY feedback_id 
+HAVING count(*) > 1;
