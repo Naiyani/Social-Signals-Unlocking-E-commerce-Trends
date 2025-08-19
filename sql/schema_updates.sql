9fc9ce42e104a6ceb4ee16dee0fb0bf9ffcad48a
Naiyani <132041238+Naiyani@users.noreply.github.com>
2025-08-19 15:24:08 -0400

    Update schema_updates.sql

--- sql/schema_updates.sql
+++ sql/schema_updates.sql
@@ -6,10 +6,41 @@ ALTER TABLE orders
   ADD COLUMN md5_hash VARCHAR(64),
   ADD COLUMN dv_load_timestamp datetime;
 
+ALTER TABLE order_items
+  ADD COLUMN md5_hash VARCHAR(64),
+  ADD COLUMN dv_load_timestamp datetime;
+  
+ALTER TABLE payments
+  ADD COLUMN md5_hash VARCHAR(64),
+  ADD COLUMN dv_load_timestamp datetime;
+
+ALTER TABLE products
+  ADD COLUMN md5_hash VARCHAR(64),
+  ADD COLUMN dv_load_timestamp datetime;
+  
+ALTER TABLE sellers
+  ADD COLUMN md5_hash VARCHAR(64),
+  ADD COLUMN dv_load_timestamp datetime;
+
+ALTER TABLE users
+  ADD COLUMN md5_hash VARCHAR(64),
+  ADD COLUMN dv_load_timestamp datetime;
+
 create table order_duplicate_archive LIKE orders;
 create table feedback_duplicate_archive LIKE feedback;
+create table order_item_duplicate_archive LIKE order_items;
+create table payment_duplicate_archive LIKE payments;
+create table products_duplicate_archive LIKE products;
+create table seller_duplicate_archive LIKE sellers;
+create table user_duplicate_archive LIKE users;
 
 create table final_feedback LIKE feedback;
+create table final_orders LIKE orders;
+create table final_order_items LIKE order_items;
+create table final_payments LIKE payments;
+create table final_products LIKE products;
+create table final_sellers LIKE sellers;
+create table final_users LIKE users;
 
 insert into final_feedback
 select * from feedback where md5_hash NOT IN (
